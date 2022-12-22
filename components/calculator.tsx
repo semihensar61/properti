5f35bfde30a6df7878d129738573efe73071e660
semih <semih@simplisales.com>
2022-12-22 05:05:22 +0300

    input restriction to get only integers

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -1,19 +1,25 @@
 
-import React, { useState, useRef } from 'react'; // we need this to make JSX compile
+import React, { useState, useRef, KeyboardEventHandler, ChangeEvent } from 'react'; // we need this to make JSX compile
 import { romanNumCalculator } from '../helpers/calculator';
 
 
 export const Claculator = ()  => {
     const [result, setResult] = useState("Your Result Will Be Displayed Here") 
     const inputRef = useRef<HTMLInputElement>(null);
-    const handleKeyPress = ()  => {
-        setResult(romanNumCalculator(inputRef))
+    const handleKeyPress = (event: any) : void  => {
+        console.log(event.key)
+        if (event.key === ".") {
+            // to prevent to enter input unless  integer 
+            event.preventDefault();
+        } else {
+            setResult(romanNumCalculator(inputRef))
+        }
     }
 
     return (
         <>
         <div className='flex flex-col  w-full '>
-            <input type="number" ref={inputRef} className='text-black w-[30%] self-center' onKeyDown={handleKeyPress}/>
+            <input type="number" min="0" step="1" ref={inputRef} className='text-black w-[30%] self-center' onKeyDown={handleKeyPress}/>
             <label className='mt-5 text-3xl'>{result}</label>
         </div>
         </>
--- helpers/calculator.tsx
+++ helpers/calculator.tsx
@@ -1,6 +1,6 @@
 // roman num calculator functon takes number and returns roman number as string
 
-import { MutableRefObject, RefObject } from "react"
+import { RefObject } from "react"
 
 export const romanNumCalculator = (x: RefObject<HTMLDivElement>) : string => {
     return "Cs"
