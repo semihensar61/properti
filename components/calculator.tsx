1672ea1877cc60f3358b8d28bc6d02039300cc26
semih <semih@simplisales.com>
2022-12-22 05:55:44 +0300

    calculator function

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -1,25 +1,19 @@
 
-import React, { useState, useRef, KeyboardEventHandler, ChangeEvent } from 'react'; // we need this to make JSX compile
+import React, { useState } from 'react'; // we need this to make JSX compile
 import { romanNumCalculator } from '../helpers/calculator';
 
 
 export const Claculator = ()  => {
     const [result, setResult] = useState("Your Result Will Be Displayed Here") 
-    const inputRef = useRef<HTMLInputElement>(null);
-    const handleKeyPress = (event: any) : void  => {
-        console.log(event.key)
-        if (event.key === ".") {
-            // to prevent to enter input unless  integer 
-            event.preventDefault();
-        } else {
-            setResult(romanNumCalculator(inputRef))
-        }
+
+    const handleChange = (event: any) : void  => {
+            setResult(romanNumCalculator(event.target.value))
     }
 
     return (
         <>
         <div className='flex flex-col  w-full '>
-            <input type="number" min="0" step="1" ref={inputRef} className='text-black w-[30%] self-center' onKeyDown={handleKeyPress}/>
+            <input type="text" pattern='\d*' maxLength={4} className='text-black w-[30%] self-center'  onChange={handleChange}/>
             <label className='mt-5 text-3xl'>{result}</label>
         </div>
         </>
--- helpers/calculator.tsx
+++ helpers/calculator.tsx
@@ -1,7 +1,27 @@
 // roman num calculator functon takes number and returns roman number as string
 
-import { RefObject } from "react"
+const Char: { [key: string]: number } = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+};
 
-export const romanNumCalculator = (x: RefObject<HTMLDivElement>) : string => {
-    return "Cs"
+export const romanNumCalculator = (x: any) : string => {
+  var romanString = '';
+  for (var i of Object.keys(Char)) {
+    var q = Math.floor(x / Char[i]);
+    x -= q * Char[i];
+    romanString += i.repeat(q);
+  }
+  return romanString;
 }
