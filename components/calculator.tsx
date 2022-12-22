eaf59f3ed57782e73f2f5909ebc582b9ac536739
semih <semih@simplisales.com>
2022-12-22 04:01:56 +0300

    result label added into calculator component - styling is done

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -6,8 +6,9 @@ import React from 'react'; // we need this to make JSX compile
 export const Claculator = ()  => {
   return (
     <>
-      <div className=''>
+      <div className='flex flex-col w-[30%]  self-center'>
         <input type="number" className='text-black'/>
+        <label className='mt-5 text-3xl'>Result Label</label>
       </div>
     </>
   )
