5719fcf7e6cee34307d991c0586e927b2f55b0cc
semih <semih@simplisales.com>
2022-12-22 04:32:02 +0300

    calculator function created and called from component

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -1,15 +1,21 @@
 
-import React from 'react'; // we need this to make JSX compile
-
+import React, { useState, useRef } from 'react'; // we need this to make JSX compile
+import { romanNumCalculator } from '../helpers/calculator';
 
 
 export const Claculator = ()  => {
-  return (
-    <>
-      <div className='flex flex-col w-[30%]  self-center'>
-        <input type="number" className='text-black'/>
-        <label className='mt-5 text-3xl'>Result Label</label>
-      </div>
-    </>
-  )
+    const [result, setResult] = useState("Your Result Will Be Displayed Here") 
+    const inputRef = useRef<HTMLInputElement>(null);
+    const handleKeyPress = ()  => {
+        setResult(romanNumCalculator(inputRef))
+    }
+
+    return (
+        <>
+        <div className='flex flex-col  w-full '>
+            <input type="number" ref={inputRef} className='text-black w-[30%] self-center' onKeyDown={handleKeyPress}/>
+            <label className='mt-5 text-3xl'>{result}</label>
+        </div>
+        </>
+    )
 }
